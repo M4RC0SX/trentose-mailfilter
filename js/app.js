8fc8016324c12fc7827d27c0a65b70ef61203d66
M4rc0SX <federicimarcosx@gmail.com>
2015-12-01 17:08:32 +0100

    First Commit, sprint1

--- js/app.js
+++ js/app.js
@@ -3,25 +3,68 @@
 
 // You can modify this object, add functions that you need
 var MailModel = {
-  /**
-   * Initialises the model with the "database" of filter rules
-   * and messages. This function is already implemented.
-   */
-   init : function(){
-     this.rules = rules;
-     this.messages = msgs;
-   }, 
-  
-   /**
-    * Filters out messages in the "database" that match the spam rules.
-    * @return an array of messages, excluding those that match the filter rules.
-    */
-    filter : function(){
-      return [];
+    /**
+     * Initialises the model with the "database" of filter rules
+     * and messages. This function is already implemented.
+     */
+    init: function () {
+        this.rules = rules;
+        this.messages = msgs;
+    },
+
+    /**
+     * Filters out messages in the "database" that match the spam rules.
+     * @return an array of messages, excluding those that match the filter rules.
+     */
+    filter: function () {
+       var filteredMessages=[];
+        this.messages.forEach(function(message){
+            var spam = false;
+            this.rules.forEach(function(rule){
+                if(message.indexOf(rule)>=0){
+                    spam=true;
+                }
+            });
+            if(!spam)filteredMessages.push(message);
+        });
+        return filteredMessages;
     }
 
-  
+
 };
+
+var MailController = {
+    init: function(){
+        MailModel.init();
+        MailView.init();
+        $(".btn-filter").on("click",function() {
+            MailView.clearMail();
+            MailView.addMail(MailModel.filter());
+        });
+        MailView.addMail(MailModel.messages);
+    }
+
+}
+
+var MailView = {
+    listTemplate: "<li>ELEMENT</li>",
+    resultUl: {},
+
+    init : function(){
+        this.resultUl=$(".result");
+        MailView.clearMail();
+    },
+
+    addMail : function(mails){
+        mails.forEach(function(mail){
+            $(MailView.resultUl).append(MailView.listTemplate.replace("ELEMENT", mail));
+        });
+    },
+
+    clearMail: function(){
+        $(this.resultUl).html("");
+    }
+}
 
 // Example of usage:
 // MailModel.init()
@@ -33,7 +76,6 @@ var MailModel = {
 // you can add here your views and controllers if you decide to do so.
 
 
-
-$(document).ready(function(){
-
+$(document).ready(function () {
+    MailController.init();
 });
